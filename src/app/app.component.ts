3fbfd4ed2b60c92c8944c7f71cb25916a7bd5611
emmah <kamauemma50@gmail.com>
2022-09-20 05:37:52 +0300

    Format the document for improve readability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  deviation!:number|null;
-  noOfTries!:number;
-  guess!:number;
-  original!:number;
-  
-  constructor(){}
+  deviation!: number | null;
+  noOfTries!: number;
+  guess!: number;
+  original!: number;
+
+  constructor() { }
 }
