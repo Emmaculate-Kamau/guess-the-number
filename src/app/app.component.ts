77ad224514e8845b67ce660844bede897c0cdc95
emmah <kamauemma50@gmail.com>
2022-09-24 14:44:03 +0300

    Finalize building of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,12 +12,28 @@ export class AppComponent {
   guess!: number | null;
   original!: number;
 
-  constructor() { }
+  constructor() {
+    this.initializeGame();
+  }
 
   initializeGame() {
     this.noOfTries = 0;
     this.original = Math.floor((Math.random() * 1000) + 1);
+    console.info("original number is ", this.original);
     this.guess = null;
     this.deviation = null;
   }
+  verifyGuess() {
+    if (this.guess !== null) {
+      this.deviation = this.original - this.guess;
+    }
+    this.noOfTries = this.noOfTries + 1;
+  }
+
+  guessInputValueChanges(inputEvent: any){
+    const inputedGuess = parseInt(inputEvent.target.value);
+
+    this.guess=inputedGuess;
+  }
 }
+
